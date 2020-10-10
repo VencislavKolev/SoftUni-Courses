2a8070f792168899a38d5b84328a6be4e4c90f17
Vencislav Kolev <venci362@gmail.com>
2020-10-10 12:19:56 +0300

    Add Fundamentals Tasks

---
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops - More Exercise/01. Sort Numbers/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace _01._Sort_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int number1 = int.Parse(Console.ReadLine());
+            //int number2 = int.Parse(Console.ReadLine());
+            //int number3 = int.Parse(Console.ReadLine());
+
+            //if (number1>=number2 && number1>=number3)
+            //{
+            //    Console.WriteLine(number1);
+            //    if (number2>=number3)
+            //    {
+            //        Console.WriteLine(number2);
+            //        Console.WriteLine(number3);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(number3);
+            //        Console.WriteLine(number2);
+            //    }
+            //}
+            //else if (number2 >= number1 && number2 >=number3)
+            //{
+            //    Console.WriteLine(number2);
+            //    if (number1 >= number3)
+            //    {
+            //        Console.WriteLine(number1);
+            //        Console.WriteLine(number3);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(number3);
+            //        Console.WriteLine(number1);
+            //    }
+            //}
+            //if (number3 >= number1 && number3 >= number2)
+            //{
+            //    Console.WriteLine(number3);
+            //    if (number2 >= number1)
+            //    {
+            //        Console.WriteLine(number2);
+            //        Console.WriteLine(number1);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(number1);
+            //        Console.WriteLine(number2);
+            //    }
+            //}
+            int number1 = int.MinValue;
+            int number2 = int.MinValue;
+            int number3 = int.MinValue;
+
+            for (int i = 1; i <= 3; i++)
+            {
+                int numberToCheck = int.Parse(Console.ReadLine());
+                if (numberToCheck > number1)
+                {
+                    number3 = number2;
+                    number2 = number1;
+                    number1 = numberToCheck;
+                }
+                else if (numberToCheck > number2)
+                {
+                    number3 = number2;
+                    number2 = numberToCheck;
+                }
+                else if (numberToCheck > number3)
+                {
+                    number3 = numberToCheck;
+                }
+
+            }
+            Console.WriteLine(number1);
+            Console.WriteLine(number2);
+            Console.WriteLine(number3);
+        }
+    }
+}
---
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops - More Exercise/03. Gaming Store/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _03._Gaming_Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double currentBalance = double.Parse(Console.ReadLine());
+            double remainingBalance = currentBalance;
+            string command = Console.ReadLine();
+            double gamePrice = 0;
+            while (command != "Game Time")
+            {
+                switch (command)
+                {
+                    case "OutFall 4": gamePrice = 39.99; break;
+                    case "CS: OG": gamePrice = 15.99; break;
+                    case "Zplinter Zell": gamePrice = 19.99; break;
+                    case "Honored 2": gamePrice = 59.99; break;
+                    case "RoverWatch": gamePrice = 29.99; break;
+                    case "RoverWatch Origins Edition": gamePrice = 39.99; break;
+
+                    default:
+                        Console.WriteLine("Not Found");
+                        command = Console.ReadLine(); continue;
+                }
+                if (remainingBalance == 0)
+                {
+                    break;
+                }
+                else if (gamePrice > remainingBalance)
+                {
+                    Console.WriteLine("Too Expensive");
+                }
+                else if (gamePrice <= remainingBalance)
+                {
+                    remainingBalance -= gamePrice;
+                    Console.WriteLine($"Bought {command}");
+                }
+                command = Console.ReadLine();
+            }
+            double moneySpentOnGames = Math.Abs(remainingBalance - currentBalance);
+            if (remainingBalance == 0)
+            {
+                Console.WriteLine("Out of money!");
+            }
+            else
+            {
+                Console.WriteLine($"Total spent: ${moneySpentOnGames:f2}. Remaining: ${remainingBalance:f2}");
+            }
+        }
+    }
+}
