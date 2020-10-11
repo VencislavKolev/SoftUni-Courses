30648655fac9048fbe08f10a49c3df9c76f3deb9
Vencislav Kolev <venci362@gmail.com>
2020-10-11 23:29:32 +0300

    Add StackQueue Lab

---
+++ C# Advanced/Stacks and Queues - Lab/2. Stack Sum/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Stack_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            Stack<int> myStack = new Stack<int>(numbers);
+
+            while (true)
+            {
+                string[] command = Console.ReadLine()
+                    .ToLower()
+                    .Split();
+                if (command[0] == "add")
+                {
+                    myStack.Push(int.Parse(command[1]));
+                    myStack.Push(int.Parse(command[2]));
+                }
+                else if (command[0] == "remove")
+                {
+                    int numbersToRemove = int.Parse(command[1]);
+                    if (myStack.Count >= numbersToRemove)
+                    {
+                        for (int i = 0; i < numbersToRemove; i++)
+                        {
+                            myStack.Pop();
+                        }
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine($"Sum: {myStack.Sum()}");
+        }
+    }
+}
---
+++ C# Advanced/Stacks and Queues - Lab/8. Traffic Jam/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _8._Traffic_Jam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            Queue<string> cars = new Queue<string>();
+            string command;
+            int totalCarsPassed = 0;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                if (command == "green")
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        if (cars.Any())
+                        {
+                            totalCarsPassed++;
+                            string currCar = cars.Dequeue();
+                            Console.WriteLine($"{currCar} passed!");
+                        }
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(command);
+                }
+            }
+            Console.WriteLine($"{totalCarsPassed} cars passed the crossroads.");
+        }
+    }
+}
