aacb1112f3c44c1b1500ab885b566e5ae7c6fc43
Vencislav Kolev <venci362@gmail.com>
2020-10-25 13:07:52 +0200

    Add Tasks

---
+++ C# Advanced/Generics - Lab/01. Box/Box.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private Stack<T> data;
+        public Box()
+        {
+            this.data = new Stack<T>();
+        }
+        public void Add(T element)
+        {
+            this.data.Push(element);
+        }
+        public T Remove()
+        {
+            T removedElement = this.data.Pop();
+            return removedElement;
+        }
+        public int Count
+        {
+            get { return this.data.Count; }
+        }
+    }
+}
---
+++ C# Advanced/Generics - Lab/GenericScale/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericScale
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> scale = new EqualityScale<int>(5, 8);
+            bool res = scale.AreEqual();
+            Console.WriteLine(res);
+        }
+    }
+}
