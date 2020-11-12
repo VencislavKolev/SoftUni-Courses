4116161ce2e7b77f4fdf978b5c8886840733407a
Vencislav Kolev <venci362@gmail.com>
2020-11-12 10:37:25 +0200

    Updated solution

---
+++ C# OOP/Exams/C# OOP Retake Exam - 16 Apr 2020/01. Structure_Skeleton/RobotService/Models/Procedures/TechCheck.cs
@@ -0,0 +1,22 @@
+﻿
+using RobotService.Models.Robots.Contracts;
+
+namespace RobotService.Models.Procedures
+{
+    public class TechCheck : Procedure
+    {
+        public TechCheck()
+        {
+        }
+
+        public override void DoService(IRobot robot, int procedureTime)
+        {
+            base.DoService(robot, procedureTime);
+
+            robot.Energy -= 8;
+            robot.IsChecked = true;
+            robot.ProcedureTime -= procedureTime;
+            this.Robots.Add(robot);
+        }
+    }
+}
