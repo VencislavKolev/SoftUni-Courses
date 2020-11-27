66bc999bca451e0c25ffd0f4b04aad8ae89dfda5
VencislavKolev <venci362@gmail.com>
2020-11-27 23:40:16 +0200

    Add Stack implementation

--- Data Structures/Linear-Data-Structures - Lab/Problem02.Stack/Stack.cs
+++ Data Structures/Linear-Data-Structures - Lab/Problem02.Stack/Stack.cs
@@ -8,34 +8,81 @@
     {
         private Node<T> _top;
 
+        public Stack()
+        {
+            this._top = null;
+            this.Count = 0;
+        }
+        public Stack(Node<T> top)
+        {
+            this._top.Next = top;
+            this.Count = 1;
+        }
         public int Count { get; private set; }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            Node<T> currNode = this._top;
+
+            while (currNode != null)
+            {
+                if (currNode.Value.Equals(item))
+                {
+                    return true;
+                }
+                currNode = currNode.Next;
+            }
+            return false;
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            this.CheckIfStackIsEmpty();
+
+            return this._top.Value;
         }
 
         public T Pop()
         {
-            throw new NotImplementedException();
+            this.CheckIfStackIsEmpty();
+
+            Node<T> topElement = this._top;
+            this._top = this._top.Next;
+            this.Count--;
+
+            return topElement.Value;
+
         }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            Node<T> toInsert = new Node<T>(item);
+            toInsert.Next = this._top;
+            this._top = toInsert;
+
+            this.Count++;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            Node<T> currNode = this._top;
+
+            while (currNode != null)
+            {
+                yield return currNode.Value;
+                currNode = currNode.Next;
+            }
         }
 
-        IEnumerator IEnumerable.GetEnumerator() 
-            => throw new NotImplementedException();
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+
+        private void CheckIfStackIsEmpty()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty.");
+            }
+        }
     }
 }
