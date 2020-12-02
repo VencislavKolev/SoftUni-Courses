a1637d4a6a24d2bdb00484e2f9373647c34d8800
VencislavKolev <venci362@gmail.com>
2020-12-02 22:41:36 +0200

    Add ReversedList

---
+++ Data Structures/Linear-Data-Structures - Exercises/03.ReversedList/ReversedList.cs
@@ -0,0 +1,148 @@
+﻿namespace Problem03.ReversedList
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class ReversedList<T> : IAbstractList<T>
+    {
+        private const int DefaultCapacity = 4;
+
+        private T[] _items;
+
+        public ReversedList()
+            : this(DefaultCapacity)
+        {
+
+        }
+
+        public ReversedList(int capacity)
+        {
+            if (capacity < 0)
+                throw new ArgumentOutOfRangeException(nameof(capacity));
+
+            this._items = new T[capacity];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                this.ValidateIndex(index);
+                return this._items[this.Count - 1 - index];
+            }
+            set
+            {
+                this.ValidateIndex(index);
+                this._items[index] = value;
+            }
+        }
+
+        public int Count { get; private set; }
+
+        public void Add(T item)
+        {
+            this.EnsureNotFull();
+            this._items[this.Count++] = item;
+        }
+
+
+        public bool Contains(T item)
+        {
+            return this.IndexOf(item) != -1;
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (this._items[this.Count - 1 - i].Equals(item))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void Insert(int index, T item)
+        {
+            this.ValidateIndex(index);
+            this.EnsureNotFull();
+
+            int indexToInsert = this.Count - index;
+            for (int i = this.Count; i > indexToInsert; i--)
+            {
+                this._items[i] = this._items[i - 1];
+            }
+
+            this._items[indexToInsert] = item;
+            this.Count++;
+        }
+
+
+        public bool Remove(T item)
+        {
+            int indexOfElement = this.IndexOf(item);
+
+            if (indexOfElement == -1)
+            {
+                return false;
+            }
+
+            this.RemoveAt(indexOfElement);
+            return true;
+        }
+
+        public void RemoveAt(int index)
+        {
+            this.ValidateIndex(index);
+
+            int indexOfElement = this.Count - 1 - index;
+            for (int i = indexOfElement; i < this.Count - 1; i++)
+            {
+                this._items[i] = this._items[i + 1];
+            }
+            this._items[this.Count - 1] = default;
+            this.Count--;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.Count - 1; i >= 0; i--)
+            {
+                yield return this._items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private void EnsureNotFull()
+        {
+            if (this.Count == this._items.Length)
+            {
+                this.Resize();
+            }
+        }
+        private void Resize()
+        {
+            int newCapacity = this._items.Length * 2;
+            T[] newArray = new T[newCapacity];
+            for (int i = 0; i < this.Count; i++)
+            {
+                newArray[i] = this._items[i];
+            }
+            this._items = newArray;
+        }
+
+        private void ValidateIndex(int index)
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+    }
+}
