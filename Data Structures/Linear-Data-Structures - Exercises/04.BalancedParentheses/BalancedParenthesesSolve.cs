7f7a811de457c73bb058e90a2af686728cabbff8
VencislavKolev <venci362@gmail.com>
2020-12-02 23:27:23 +0200

    Add BalancedParentheses

---
+++ Data Structures/Linear-Data-Structures - Exercises/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -0,0 +1,48 @@
+﻿namespace Problem04.BalancedParentheses
+{
+    using System.Collections.Generic;
+
+    public class BalancedParenthesesSolve : ISolvable
+    {
+        public bool AreBalanced(string parentheses)
+        {
+            if (parentheses.Length % 2 != 0)
+            {
+                return false;
+            }
+
+            Stack<char> openBrackets = new Stack<char>();
+
+            foreach (char bracket in parentheses)
+            {
+                char expBracket = default;
+
+                switch (bracket)
+                {
+                    case ')':
+                        expBracket = '(';
+                        break;
+                    case ']':
+                        expBracket = '[';
+                        break;
+                    case '}':
+                        expBracket = '{';
+                        break;
+                    default:
+                        openBrackets.Push(bracket);
+                        break;
+                }
+                if (expBracket == default)
+                {
+                    continue;
+                }
+                if (openBrackets.Pop() != expBracket)
+                {
+                    return false;
+                }
+            }
+
+            return openBrackets.Count == 0;
+        }
+    }
+}
