19a00a5ef810219444b134a135f69a1531b369ab
VencislavKolev <venci362@gmail.com>
2020-12-04 23:41:56 +0200

    Add Task

--- C# DB/MS SQL Server/Exercises - Data Aggregation/SoftUniDB.sql
+++ C# DB/MS SQL Server/Exercises - Data Aggregation/SoftUniDB.sql
@@ -14,3 +14,16 @@ SELECT
 	FROM Employees
 		WHERE HireDate > '01/01/2000' AND DepartmentID IN (2,5,7)
 	GROUP BY DepartmentID
+
+--TASK 15
+
+
+--TASK 16
+SELECT 
+	DepartmentID,
+	MAX(Salary) AS MaxSalary
+	FROM Employees
+GROUP BY DepartmentID
+HAVING MAX(Salary) NOT BETWEEN 30000 AND 70000
+
+--TASK 17
