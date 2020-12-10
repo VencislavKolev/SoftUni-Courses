9b49d9982e0b5063cb3b8947712f413b3d19768b
VencislavKolev <venci362@gmail.com>
2020-12-10 20:29:40 +0200

    Added set of tests

--- C# OOP/Exams/C# OOP Retake Exam - 22 August 2020/UnitTests-Skeleton/TheRace.Tests/RaceEntryTests.cs
+++ C# OOP/Exams/C# OOP Retake Exam - 22 August 2020/UnitTests-Skeleton/TheRace.Tests/RaceEntryTests.cs
@@ -170,3 +170,149 @@ namespace TheRace.Tests
         }
     }
 }
+
+// Latest Try
+using System;
+using NUnit.Framework;
+using TheRace;
+
+namespace TheRace.Tests
+{
+    public class RaceEntryTests
+    {
+
+        private RaceEntry raceEntry;
+        [SetUp]
+        public void Setup()
+        {
+            raceEntry = new RaceEntry();
+        }
+
+        //UnitCar Test
+        [Test]
+        public void UnitCarCtorShouldWorkCorrectly()
+        {
+            //Arrange
+            string eModel = "Corsa";
+            int eHP = 80;
+            double eCubCentimeters = 1200;
+
+            //Act
+            UnitCar car = new UnitCar(eModel, eHP, eCubCentimeters);
+            //Assert
+            Assert.AreEqual(eModel, car.Model);
+            Assert.AreEqual(eHP, car.HorsePower);
+            Assert.AreEqual(eCubCentimeters, car.CubicCentimeters);
+        }
+
+        // UnitDriver Tests
+        [Test]
+        public void UnitDriverCtorShouldWorkCorrectly()
+        {
+            //Arrange
+            string eName = "ValidName";
+            string eModel = "Corsa";
+            int eHP = 80;
+            double eCubCentimeters = 1200;
+            UnitCar car = new UnitCar(eModel, eHP, eCubCentimeters);
+            //Act
+            UnitDriver unitDriver = new UnitDriver(eName, car);
+            //Assert
+            Assert.AreEqual(eName, unitDriver.Name);
+        }
+        [Test]
+        public void NameShouldThrowExceptionWhenNull()
+        {
+            //Arrange
+            string eModel = "Corsa";
+            int eHP = 80;
+            double eCubCentimeters = 1200;
+            UnitCar car = new UnitCar(eModel, eHP, eCubCentimeters);
+            //Act
+
+            //Assert
+            Assert.That(() =>
+            {
+                UnitDriver unitDriver = new UnitDriver(null, car);
+            }, Throws.Exception.InstanceOf<ArgumentNullException>());
+        }
+
+        //Race Entry Tests
+        [Test]
+        public void TestEntryCtorShouldWorkCorrectly()
+        {
+            RaceEntry raceEntry = new RaceEntry();
+            Assert.IsNotNull(raceEntry);
+        }
+
+        [Test]
+        public void AddDriverShouldThrowExceptionWhenDriverIsNull()
+        {
+            //Assert
+            Assert.That(() =>
+            {
+                //Act
+                raceEntry.AddDriver(null);
+            }, Throws.Exception.InstanceOf<InvalidOperationException>()
+            .With.Message.EqualTo("Driver cannot be null."));
+        }
+
+        [Test]
+        public void AddDriverShouldThrowExceptionWhenDriverAlreadyExists()
+        {
+            //Arrange
+            UnitDriver unitDriver = new UnitDriver("First", new UnitCar("Model", 100, 1000));
+            this.raceEntry.AddDriver(unitDriver);
+
+            //Assert
+            Assert.That(() =>
+            {
+                this.raceEntry.AddDriver(unitDriver);
+            }, Throws.Exception.InstanceOf<InvalidOperationException>()
+            .With.Message.EqualTo($"Driver {unitDriver.Name} is already added."));
+        }
+
+        [Test]
+        public void AddDriverShouldWorkCorrectly()
+        {
+            //Arrange
+            UnitDriver unitDriver = new UnitDriver("First", new UnitCar("Model", 100, 1000));
+            string expResult = $"Driver {unitDriver.Name} added in race.";
+            int expCount = 1;
+            //Act
+            string result = this.raceEntry.AddDriver(unitDriver);
+            //Assert
+            Assert.AreEqual(expResult, result);
+            Assert.AreEqual(expCount, this.raceEntry.Counter);
+        }
+
+        [Test]
+        public void CalculateAverageHorsePowerShouldThrowExceptionWhenNotEnoughParticipants()
+        {
+            //Assert
+            Assert.That(() =>
+            {
+                //Act
+                this.raceEntry.CalculateAverageHorsePower();
+            }, Throws.Exception.InstanceOf<InvalidOperationException>()
+            .With.Message.EqualTo("The race cannot start with less than 2 participants."));
+        }
+        [Test]
+        public void CalculateAverageHorsePowerShouldReturnValidResult()
+        {
+            //Arrange
+            UnitDriver first = new UnitDriver("First", new UnitCar("Model", 100, 1000));
+            UnitDriver second = new UnitDriver("Second", new UnitCar("Model2", 50, 1000));
+            this.raceEntry.AddDriver(first);
+            this.raceEntry.AddDriver(second);
+            double expResult = (100 + 50) / 2;
+
+            //Act
+            double result = this.raceEntry.CalculateAverageHorsePower();
+
+            //Assert
+            Assert.AreEqual(expResult, result);
+        }
+
+    }
+}
