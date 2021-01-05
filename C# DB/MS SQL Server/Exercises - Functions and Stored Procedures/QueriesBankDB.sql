97dbab6e4af40c521c46312173d4efbb916e76b6
VencislavKolev <venci362@gmail.com>
2021-01-05 23:07:35 +0200

    Task 10

--- C# DB/MS SQL Server/Exercises - Functions and Stored Procedures/QueriesBankDB.sql
+++ C# DB/MS SQL Server/Exercises - Functions and Stored Procedures/QueriesBankDB.sql
@@ -7,3 +7,14 @@ AS
 GO
 
 --Task 10
+CREATE OR ALTER PROCEDURE usp_GetHoldersWithBalanceHigherThan(@Value DECIMAL(18,4))
+AS
+	SELECT FirstName, LastName
+	FROM AccountHolders AS ah
+	JOIN Accounts AS a ON ah.Id = a.AccountHolderId
+	GROUP BY FirstName, LastName
+		HAVING SUM(Balance) > @Value
+	ORDER BY FirstName ASC, LastName ASC
+GO
+
+EXEC usp_GetHoldersWithBalanceHigherThan 2
