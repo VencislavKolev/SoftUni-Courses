1eb78d267f97e0a4c724a89402171dae58ba59c0
VencislavKolev <venci362@gmail.com>
2021-01-12 16:36:22 +0200

    add task

---
+++ C# DB/Entity Framework Core/ADO.NET Exercices/P02.Villain Names/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace P02.Villain_Names
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using SqlConnection sqlConnection = new SqlConnection("Server=.;Database=MinionsDB;Integrated Security=true;");
+            sqlConnection.Open();
+
+            SqlCommand sqlCommand = new SqlCommand(
+                "SELECT v.Name AS VillainName, COUNT(mv.VillainId) AS MinionsCount " +
+                "FROM Villains AS v " +
+                "LEFT JOIN MinionsVillains AS mv ON v.Id = mv.VillainId " +
+                "GROUP BY v.Name, mv.VillainId " +
+                "HAVING COUNT(mv.VillainId " +
+                "ORDER BY mv.VillainId DESC", sqlConnection);
+            using SqlDataReader reader = sqlCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                string villainName = reader["VillainName"].ToString();
+                string minionsCount = reader["MinionsCount"].ToString();
+                Console.WriteLine($"{villainName} - {minionsCount}");
+            }
+        }
+    }
+}
