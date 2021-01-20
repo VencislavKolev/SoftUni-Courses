fd4b8886f93fb70bde03f967730482850476a40c
VencislavKolev <venci362@gmail.com>
2021-01-20 20:05:08 +0200

    Add Football Betting System Database

---
+++ C# DB/Entity Framework Core/Entity Relations - Exercise/FootballSystem/P03_FootballBetting.Data/ConfigurationData.cs
@@ -0,0 +1,8 @@
+﻿
+namespace P03_FootballBetting.Data
+{
+    internal static class ConfigurationData
+    {
+        internal static string ConnectionString = @"Server=.;Database=FootballBettingSystem;Integrated Security=True;";
+    }
+}
