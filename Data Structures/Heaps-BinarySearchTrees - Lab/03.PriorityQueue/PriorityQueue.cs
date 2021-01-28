d7a6d5c6be9a65a2150ff6df525537b3d2b8359a
VencislavKolev <venci362@gmail.com>
2021-01-28 16:27:10 +0200

    Add PriorityQueue Impl

--- Data Structures/Heaps-BinarySearchTrees - Lab/03.PriorityQueue/PriorityQueue.cs
+++ Data Structures/Heaps-BinarySearchTrees - Lab/03.PriorityQueue/PriorityQueue.cs
@@ -1,25 +1,121 @@
 ﻿namespace _03.PriorityQueue
 {
     using System;
+    using System.Collections.Generic;
 
     public class PriorityQueue<T> : IAbstractHeap<T>
         where T : IComparable<T>
     {
-        public int Size { get { throw new NotImplementedException(); } }
+        private List<T> _elements;
 
-        public T Dequeue()
+        public PriorityQueue()
         {
-            throw new NotImplementedException();
+            this._elements = new List<T>();
+        }
+        public int Size => this._elements.Count;
+
+        public T Peek()
+        {
+            this.EnsureNotEmpty();
+
+            return this._elements[0];
         }
 
         public void Add(T element)
         {
-            throw new NotImplementedException();
+            this._elements.Add(element);
+
+            this.HeapifyUp();
         }
 
-        public T Peek()
+        public T Dequeue()
         {
-            throw new NotImplementedException();
+            T firstElement = this.Peek();
+            this.Swap(0, this.Size - 1);
+            this._elements.RemoveAt(this.Size - 1);
+
+            this.HeapifyDown();
+
+            return firstElement;
+        }
+
+        private void HeapifyUp()
+        {
+            int index = this.Size - 1;
+            int parentIndex = this.GetParentIndex(index);
+
+            while (this.IsValidIndex(index) && this.IsGreater(index, parentIndex))
+            {
+                this.Swap(index, parentIndex);
+                index = parentIndex;
+                parentIndex = this.GetParentIndex(index);
+            }
+        }
+
+        private void HeapifyDown()
+        {
+            int index = 0;
+            int leftChildIndex = this.GetLeftChildIndex(index);
+            while (this.IsValidIndex(leftChildIndex) && this.IsLess(index, leftChildIndex))
+            {
+                int toSwapWith = leftChildIndex;
+                int rightChildIndex = this.GetRightChildIndex(index);
+
+                if (this.IsValidIndex(rightChildIndex) && this.IsLess(toSwapWith, rightChildIndex))
+                {
+                    toSwapWith = rightChildIndex;
+                }
+
+                this.Swap(index, toSwapWith);
+
+                index = toSwapWith;
+                leftChildIndex = this.GetLeftChildIndex(index);
+            }
+        }
+
+        private void Swap(int index, int newIndex)
+        {
+            T temp = this._elements[index];
+            this._elements[index] = this._elements[newIndex];
+            this._elements[newIndex] = temp;
+        }
+
+        private bool IsValidIndex(int index)
+        {
+            return index < this.Size;
+        }
+        private bool IsGreater(int index, int parentIndex)
+        {
+            return this._elements[index]
+                .CompareTo(this._elements[parentIndex]) > 0;
+        }
+
+        private bool IsLess(int index, int parentIndex)
+        {
+            return this._elements[index]
+                .CompareTo(this._elements[parentIndex]) < 0;
+        }
+        private int GetParentIndex(int index)
+        {
+            return (index - 1) / 2;
+        }
+
+        private int GetLeftChildIndex(int index)
+        {
+            return 2 * index + 1;
+        }
+
+        private int GetRightChildIndex(int index)
+        {
+            return 2 * index + 2;
+        }
+
+        private void EnsureNotEmpty()
+        {
+            if (this._elements.Count == 0)
+            {
+                throw new InvalidOperationException("The heap is empty!");
+            }
         }
     }
 }
