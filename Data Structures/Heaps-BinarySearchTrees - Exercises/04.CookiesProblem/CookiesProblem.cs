6da8db41991803b4ed1bda3c66229cd8f232bd91
VencislavKolev <venci362@gmail.com>
2021-01-29 15:45:03 +0200

    Add Task 4

--- Data Structures/Heaps-BinarySearchTrees - Exercises/04.CookiesProblem/CookiesProblem.cs
+++ Data Structures/Heaps-BinarySearchTrees - Exercises/04.CookiesProblem/CookiesProblem.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using Wintellect.PowerCollections;
 
 namespace _04.CookiesProblem
 {
@@ -6,7 +6,29 @@ namespace _04.CookiesProblem
     {
         public int Solve(int k, int[] cookies)
         {
-            throw new NotImplementedException();
+            var priorityQueue = new OrderedBag<int>();
+
+            foreach (var cookie in cookies)
+            {
+                priorityQueue.Add(cookie);
+            }
+
+            int count = 0;
+            int currLeastSweetCookie = priorityQueue.GetFirst();
+
+            while (currLeastSweetCookie < k && priorityQueue.Count > 1)
+            {
+                int firstLeastSweet = priorityQueue.RemoveFirst();
+                int secondLeastSweet = priorityQueue.RemoveFirst();
+
+                int combined = firstLeastSweet + 2 * secondLeastSweet;
+                priorityQueue.Add(combined);
+
+                currLeastSweetCookie = priorityQueue.GetFirst();
+                count++;
+            }
+
+            return currLeastSweetCookie < k ? -1 : count;
         }
     }
 }
