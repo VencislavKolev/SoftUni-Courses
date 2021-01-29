6055151ff0598923aa65a94870c47d1d0beab5e9
VencislavKolev <venci362@gmail.com>
2021-01-29 16:30:42 +0200

    Add Task 5

--- Data Structures/Heaps-BinarySearchTrees - Exercises/05.TopView/BinaryTree.cs
+++ Data Structures/Heaps-BinarySearchTrees - Exercises/05.TopView/BinaryTree.cs
@@ -1,15 +1,17 @@
 ﻿namespace _05.TopView
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
+    using System.Collections.Generic;
 
     public class BinaryTree<T> : IAbstractBinaryTree<T>
         where T : IComparable<T>
     {
         public BinaryTree(T value, BinaryTree<T> left, BinaryTree<T> right)
         {
-            throw new NotImplementedException();
+            this.Value = value;
+            this.LeftChild = left;
+            this.RightChild = right;
         }
 
         public T Value { get; set; }
@@ -20,7 +22,37 @@
 
         public List<T> TopView()
         {
-            throw new NotImplementedException();
+            var offsetToValueLevel = new SortedDictionary<int, KeyValuePair<T, int>>();
+
+            this.FillDictionaryDfs(this, 0, 1, offsetToValueLevel);
+
+            return offsetToValueLevel.Values
+                                     .Select(x => x.Key)
+                                     .ToList();
+        }
+
+        private void FillDictionaryDfs(BinaryTree<T> subtree,
+            int offset,
+            int level,
+            SortedDictionary<int, KeyValuePair<T, int>> offsetToValueLevel)
+        {
+            if (subtree == null)
+            {
+                return;
+            }
+
+            if (!offsetToValueLevel.ContainsKey(offset))
+            {
+                offsetToValueLevel[offset] = new KeyValuePair<T, int>(subtree.Value, level);
+            }
+
+            if (level < offsetToValueLevel[offset].Value)
+            {
+                offsetToValueLevel[offset] = new KeyValuePair<T, int>(subtree.Value, level);
+            }
+
+            this.FillDictionaryDfs(subtree.LeftChild, offset - 1, level - 1, offsetToValueLevel);
+            this.FillDictionaryDfs(subtree.RightChild, offset + 1, level + 1, offsetToValueLevel);
         }
     }
 }
