ef54dbb9b2e32d4f5b1d8741da8f1fa4001ef2a7
VencislavKolev <venci362@gmail.com>
2021-03-18 18:34:06 +0200

    initial commit

---
+++ Java DB/SpringData/04-Hibernate-CodeFirst-Exercise/src/main/java/App.java
@@ -0,0 +1,18 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class App {
+    public static final String GRINGOTTS_PU = "gringotts";
+    public static final String SALES_PU = "sales";
+    public static final String UNIVERSITY_SYSTEM_PU = "university_system";
+
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory =
+                Persistence.createEntityManagerFactory(UNIVERSITY_SYSTEM_PU);
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        Engine engine = new Engine(entityManager);
+        engine.run();
+    }
+}
