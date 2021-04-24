8b334ee2e09177ae90095bf819f62fa29a0a84ab
VencislavKolev <venci362@gmail.com>
2021-04-24 18:00:25 +0300

    initial commit

---
+++ Java DB/SpringData/08-AutoMappingObjects-Exercise/src/test/java/com/springdata/gamestore/AutoMappingObjectsExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdata.gamestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoMappingObjectsExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
