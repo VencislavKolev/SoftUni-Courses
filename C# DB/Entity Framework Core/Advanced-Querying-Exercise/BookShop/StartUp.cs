d8f4cd82e773a67d08a4edb137e8174b36b1787f
VencislavKolev <venci362@gmail.com>
2021-06-22 23:33:20 +0300

    Add Task 2, 3 and 4

--- C# DB/Entity Framework Core/Advanced-Querying-Exercise/BookShop/StartUp.cs
+++ C# DB/Entity Framework Core/Advanced-Querying-Exercise/BookShop/StartUp.cs
@@ -2,6 +2,8 @@
 {
     using System;
     using System.Linq;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Text;
     using BookShop.Models.Enums;
     using Data;
     using Initializer;
@@ -14,14 +16,49 @@
             using var db = new BookShopContext();
             //DbInitializer.ResetDatabase(db);
 
-            //----------------2.Age Restriction----------------
-            string command = Console.ReadLine().ToLower();
-            Console.WriteLine(GetBooksByAgeRestriction(db, command));
+            //----------------2. Age Restriction----------------
+            //string command = Console.ReadLine().ToLower();
+            //Console.WriteLine(GetBooksByAgeRestriction(db, command));
 
-            //----------------2.Age Restriction----------------
+            ////----------------3. Golden Books----------------
+            //Console.WriteLine(GetGoldenBooks(db));
+
+            //----------------4. Books by Price----------------
+            Console.WriteLine(GetBooksByPrice(db));
         }
 
-        private static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            var result = context.Books
+                .Where(x => x.Price > 40)
+                .Select(x => new
+                {
+                    Title = x.Title,
+                    Price = x.Price
+                })
+                .OrderByDescending(x => x.Price)
+                .ToArray();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var book in result)
+            {
+                sb.AppendLine($"{book.Title} - ${book.Price}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var result = context.Books
+                .Where(x => x.EditionType == EditionType.Gold && x.Copies < 5000)
+                .OrderBy(x => x.BookId)
+                .Select(x => x.Title)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, result);
+        }
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
         {
             var cmdEnum = (AgeRestriction)Enum.Parse(typeof(AgeRestriction), command, true);
 
