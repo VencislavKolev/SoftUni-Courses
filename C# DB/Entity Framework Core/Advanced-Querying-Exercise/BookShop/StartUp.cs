8e05adadcce7a704ac9aa5d771cd1c3773ad8562
VencislavKolev <venci362@gmail.com>
2021-06-23 00:18:58 +0300

    Add task 5, 6 and 7

--- C# DB/Entity Framework Core/Advanced-Querying-Exercise/BookShop/StartUp.cs
+++ C# DB/Entity Framework Core/Advanced-Querying-Exercise/BookShop/StartUp.cs
@@ -1,6 +1,7 @@
 ﻿namespace BookShop
 {
     using System;
+    using System.Globalization;
     using System.Linq;
     using System.Security.Cryptography.X509Certificates;
     using System.Text;
@@ -23,8 +24,72 @@
             ////----------------3. Golden Books----------------
             //Console.WriteLine(GetGoldenBooks(db));
 
-            //----------------4. Books by Price----------------
-            Console.WriteLine(GetBooksByPrice(db));
+            ////----------------4. Books by Price----------------
+            //Console.WriteLine(GetBooksByPrice(db));
+
+            ////----------------5. Not Released In----------------
+            //int year = int.Parse(Console.ReadLine());
+            //Console.WriteLine(GetBooksNotReleasedIn(db, year));
+
+            ////----------------6. Book Titles by Category----------------
+            //string command = Console.ReadLine();
+            //Console.WriteLine(GetBooksByCategory(db, command));
+
+            //----------------7. Released Before Date----------------
+            string command = Console.ReadLine();
+            Console.WriteLine(GetBooksReleasedBefore(db, command));
+
+        }
+
+        public static string GetBooksReleasedBefore(BookShopContext context, string date)
+        {
+            DateTime dateTime = DateTime.ParseExact(date, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+
+            var result = context.Books
+                .Where(x => x.ReleaseDate < dateTime)
+                .Select(x => new
+                {
+                    Title = x.Title,
+                    EditionType = x.EditionType,
+                    Price = x.Price,
+                    ReleaseDate = x.ReleaseDate
+                })
+                .OrderByDescending(x => x.ReleaseDate)
+                .ToArray();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in result)
+            {
+                sb.AppendLine($"{item.Title} - {item.EditionType} - ${item.Price} ");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetBooksByCategory(BookShopContext context, string input)
+        {
+            var categories = input.Split(new char[] { ' ' }).ToArray();
+            var result = context.Books
+                .Where(x => x.BookCategories.Any(x => categories.Contains(x.Category.Name.ToLower())))
+                .Select(x => x.Title)
+                .OrderBy(x => x)
+                .ToArray();
+
+            return String.Join(Environment.NewLine, result);
+        }
+
+        public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+        {
+            //DateTime date = new DateTime(year, 1, 1);
+
+            var result = context.Books
+                .Where(x => x.ReleaseDate.Value.Year != year)
+                .OrderBy(x => x.BookId)
+                .Select(x => x.Title)
+                .ToArray();
+
+            return String.Join(Environment.NewLine, result);
         }
 
         public static string GetBooksByPrice(BookShopContext context)
