2dbbcb098aa2bbdeb5cd9024e3d4a038a0391f5a
VencislavKolev <venci362@gmail.com>
2021-06-28 16:53:19 +0300

    Add Models

---
+++ C# DB/Entity Framework Core/BestPracticesAndArchitecture-Lab/RealEstate/RealEstate.Models/BuildingType.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RealEstate.Models
+{
+    public class BuildingType
+    {
+        public BuildingType()
+        {
+            this.Properties = new HashSet<RealEstateProperty>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public virtual ICollection<RealEstateProperty> Properties { get; set; }
+    }
+}
---
+++ C# DB/Entity Framework Core/BestPracticesAndArchitecture-Lab/RealEstate/RealEstate.Models/Tag.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RealEstate.Models
+{
+    public class Tag
+    {
+        public Tag()
+        {
+            this.Tags = new HashSet<RealEstatePropertyTag>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public ICollection<RealEstatePropertyTag> Tags { get; set; }
+    }
+}
