5e2f5aa61ee3e407e158446e99f02a56a71f7c67
Vencislav Kolev <venci362@gmail.com>
2022-10-08 22:43:43 +0300

    Add task 7 and 8

--- JavaScript/JS-Advanced/04-DOM Introduction-Exercise/07. Hell's Kitchen/app.js
+++ JavaScript/JS-Advanced/04-DOM Introduction-Exercise/07. Hell's Kitchen/app.js
@@ -1,27 +1,61 @@
 function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
-   let data = {};
+   let restaurants = [];
 
    function onClick() {
       let inputs = JSON.parse(document.querySelector('#inputs textarea').value);
       for (const input of inputs) {
+         let isNewRestaurant = true;
          let split = input.split(' - ');
-         let restaurant = {};
-         restaurant.name = split[0];
-         restaurant.employees = [];
+         let restaurantName = split[0];
+         let currentRestaurant;
+         if (restaurants[restaurantName]) {
+            currentRestaurant = restaurants[restaurantName]
+            isNewRestaurant = false;
+         } else {
+            currentRestaurant = {
+               name: restaurantName,
+               employees: [],
+               totalSalary: 0,
+               averageSalary: 0,
+               bestSalary: 0
+            }
+         }
 
          let workersSalaries = split[1].split(', ');
-         let totalSalary = 0;
          for (const data of workersSalaries) {
             let name = data.split(' ')[0];
             let salary = Number(data.split(' ')[1]);
+            if (salary > currentRestaurant.bestSalary) {
+               currentRestaurant.bestSalary = salary
+            }
             let employee = { name, salary };
-            restaurant.employees.push(employee);
-            totalSalary += salary;
+            currentRestaurant.employees.push(employee);
+            currentRestaurant.totalSalary += salary;
          }
-         let averageSalary = totalSalary / restaurant.employees.length;
-         restaurant.averageSalary = averageSalary;
-         console.log(restaurant);
+         let averageSalary = currentRestaurant.totalSalary / currentRestaurant.employees.length;
+         currentRestaurant.averageSalary = parseFloat(averageSalary.toFixed(2));
+         if (isNewRestaurant) {
+            restaurants.push(currentRestaurant)
+         }
+         console.log(currentRestaurant);
       }
+      restaurants.sort((x, y) => y.averageSalary - x.averageSalary)
+      const winner = restaurants[0];
+      winner.employees.sort((x, y) => y.salary - x.salary);
+      let firstOutput = `Name: ${winner.name} Average Salary: ${winner.averageSalary.toFixed(2)} Best Salary: ${winner.bestSalary.toFixed(2)}`;
+      console.log(firstOutput);
+
+      let secondOutput = '';
+      for (const e of winner.employees) {
+         secondOutput = secondOutput.concat(`Name: ${e.name} With Salary: ${e.salary} `);
+      }
+      console.log(secondOutput);
+      let paragraphs = document.querySelectorAll('p');
+      paragraphs[0].textContent = firstOutput
+      paragraphs[1].textContent = secondOutput
    }
 }
+
+// solve('["PizzaHut - Peter 500, George 300, Mark 800","TheLake - Bob 1300, Joe 780, Jane 660"]')
+// solve('["Mikes - Steve 1000, Ivan 200, Paul 800","Fleet - Maria 850, Janet 650"]')
--- JavaScript/JS-Advanced/04-DOM Introduction-Exercise/08. Generate Report/report.js
+++ JavaScript/JS-Advanced/04-DOM Introduction-Exercise/08. Generate Report/report.js
@@ -1,3 +1,22 @@
 function generateReport() {
-    //TODO
+    let checkboxes = document.querySelectorAll('input[type=checkbox]');
+    let rows = document.querySelectorAll("tbody tr");
+    let result = [];
+
+    for (let i = 0; i < rows.length; i++) {
+        let info = {};
+        for (let j = 0; j < checkboxes.length; j++) {
+            if (checkboxes[j].checked) {
+                let columnName = checkboxes[j].name;
+                let value = rows[i].textContent
+                    .split("\n")
+                    .map(x => x.trim())
+                    .filter(x => x !== "")[j];
+                info[columnName] = value;
+            }
+        }
+        result.push(info);
+    }
+    let outputElement = document.querySelector("#output");
+    outputElement.value = JSON.stringify(result, null, 2);
 }
