a8401f003cfd0e844a73f364316f329efe54b177
Vencislav Kolev <venci362@gmail.com>
2022-10-15 20:55:09 +0300

    Add task 7

--- JavaScript/JS-Advanced/05-DOM-Manipulation-Events-Lab/07. Shopping-Cart/solution.js
+++ JavaScript/JS-Advanced/05-DOM-Manipulation-Events-Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,40 @@
 function solve() {
-   //TODO...
+   let addProductButtonElements = Array.from(document.getElementsByClassName('product-add'));
+   let checkoutButtonElement = document.querySelector('button[class=checkout]');
+   let textAreaElement = document.querySelector('textarea');
+
+   let products = [];
+
+   addProductButtonElements.forEach(addButton => {
+      addButton.addEventListener('click', addToCartEventHandler);
+   });
+
+   checkoutButtonElement.addEventListener('click', checkoutEventHandler, { once: true });
+
+   function addToCartEventHandler(e) {
+      if (!e.currentTarget.disabled) {
+         let productDivElement = e.currentTarget.parentElement;
+
+         let title = productDivElement.querySelector('.product-title').textContent;
+         let price = Number(productDivElement.querySelector('.product-line-price').textContent).toFixed(2);
+
+         const product = { title, price };
+         products.push(product);
+
+         textAreaElement.value += `Added ${title} for ${price} to the cart.\n`;;
+      }
+   }
+
+   function checkoutEventHandler() {
+      addProductButtonElements.forEach(btn => {
+         btn.disabled = true;
+      })
+
+      const productTitles = [...new Set(products.map(x => x.title))];
+      const totalPrice = products
+         .map(x => Number(x.price))
+         .reduce((acc, cur) => acc += cur)
+
+      textAreaElement.value += `You bought ${productTitles.join(', ')} for ${totalPrice.toFixed(2)}.`
+   }
 }
